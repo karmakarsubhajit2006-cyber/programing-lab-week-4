05a42315b4e720384eff44a0f87b113aa0c05144
karmakarsubhajit2006-cyber <karmakarsubhajit2006@gmail.com>
2025-09-02 11:17:57 +0530

    Add files via upload

---
+++ assignment19.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char coin;
+
+
+    printf("Enter the result of the coin toss (H for Head, T for Tail): ");
+    scanf(" %c", &coin); 
+
+    
+    if (coin == 'H' || coin == 'h') {
+        printf("It is Head.\n");
+    }
+    else if (coin == 'T' || coin == 't') {
+        printf("It is Tail.\n");
+    }
+    else {
+        printf("Invalid input! Please enter H or T.\n");
+    }
+
+    return 0;
+}
